1a3ed6922ae841324cf0b7f8ec106b4d816b44fb
husheng <1606272735@qq.com>
2026-01-04 17:56:39 +0800

    update

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,13 @@
 # -------------------------- 测试代码（可验证运行） --------------------------
 if __name__ == "__main__":
-    from factorio.satisfactory.formula import formula, nx, RecipeGNN, torch, RecipeGNN_V2, device
+    from factorio.satisfactory.formula import formula, nx, RecipeGNN, torch, RecipeGNN_V2, device, RecipeGNN_V3
     base_weight={
         "水": 0, "铁矿石": 1/921, "石灰石": 1/693, "铜矿石": 1/369, "钦金矿石": 1/150, "煤": 1/423,
         "原油": 1/126, "硫磺": 1/108, "铝土矿": 1/123, "粗石英": 1/135, "铀": 1/21, "SAM物质": 1/102, "氮气": 1/120
+    }
+    target_weight={
+        "奇点电池": 1,
+        "镄燃料棒": 2
     }
     # 1. 构建示例配方图
     model = RecipeGNN(formula.g, target_item = "镄燃料棒")
